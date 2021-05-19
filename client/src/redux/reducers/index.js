3479adee1b2efe60bb7378801c60f064ccd17d3d
giraff <hse05105@naver.com>
2021-05-19 18:50:48 +0900

    Login 관련 타입이름, auth에 관련된 리듀서 선언

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
+import authReducer from './authReducer';
 
-
-const createRootReducer = (history) => combineReducers({
-  // combineReducer는 redux store 내부의 여러 상태를 가진 리듀서를 관리하기 위해 사용
-  // 반환값 : reducer 함수
-  router: connectRouter(history)
+// combineReducer는 redux store 내부의 여러 상태를 가진 리듀서를 관리하기 위해 사용
+// 반환값 : reducer 함수
+const createRootReducer = (history) =>
+combineReducers({
+  router: connectRouter(history),
+  auth: authReducer
 })
 
 export default createRootReducer;
