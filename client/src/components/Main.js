88770b7fd9fc1b1dbfb27eaa913fbd09ab86f2c1
giraff <hse05105@naver.com>
2021-05-22 22:36:28 +0900

    Main 수정(로그아웃 기능 보이게)

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,18 +1,48 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+import { LOGOUT_REQUEST } from '../redux/types';
 
 const Main = () => {
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const onLogout = useCallback(() => {
+    // 메모제이션된 콜백을 반환한다.
+    dispatch({
+      type: LOGOUT_REQUEST
+    })
+  }, [dispatch]);
+
+  const authLink = (
+    <Fragment>
+      <nav>
+        <form>
+          <Link onClick={onLogout} to="#">
+            <button>logout</button>
+          </Link>
+        </form>
+      </nav>
+    </Fragment>
+  );
+  
+  const guestLink = (
+    <Fragment>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">Register</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </Fragment>
+  );
   return (
     <Fragment>
-      <h1>Hello</h1>
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-      </ul>
+      <h1>Qallah</h1>
+      {isAuthenticated ? authLink : guestLink}
     </Fragment>
   );
 };
