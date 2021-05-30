d7d9d2ac44e4ac27842ae68f9d08383267433b99
coldexpression <uni_vers_@naver.com>
2021-05-30 21:34:44 +0900

    [Issue] 내가보는 나 - 첫 화면에서 첫 질문이 로딩되지 않는 이슈 해결

--- client/src/components/questiontome/ToMeDetail.js
+++ client/src/components/questiontome/ToMeDetail.js
@@ -1,30 +1,46 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { TOME_REQUEST } from "../../redux/types";
+import { TOME_REQUEST,TOME_SUCCESS } from "../../redux/types";
 
 const QuestionToMeDetail = () => {
     const ToMeObj = useSelector((state) => state.tomedetail.payload);
     const ToMeChk = useSelector((state) => state.tomedetail.isToMe);
 
     const dispatch = useDispatch();
-
-    useEffect(() => {
-        console.log("ToMeDetail Render");
-        dispatch({
-            type: TOME_REQUEST
-        });
-      }, []);
-
     
     let next_button = false;
     let prev_button = false;
 
     const [form,setValues] = useState({
         question_seq: 0,
-        question_context: ToMeChk == true? ToMeObj.data[0].question_content : "" ,
+        question_context: "",
         question_answer: ""
     })
+
+    // 첫 번째 useEffect는 DB에서 질문을 조회하기 위한 것
+    useEffect(() => {
+        console.log("ToMeDetail Render");
+        dispatch({
+            type: TOME_REQUEST
+        });
+        console.log("렌더 종료",ToMeChk);
+      }, []);  
+    
+    // 두 번째 useEffect는 첫 화면에서 첫 질문이 로딩되지 않는 이슈의 해결을 위한 것
+    useEffect(() => {
+        if(ToMeChk)
+        {
+            setValues({
+                ...form,
+                question_context: ToMeObj.data[question_seq].question_content
+            });
+        } else {
+            console.log(ToMeChk)
+        }
+    }, [ToMeChk]);
+
 
     const { question_seq, question_context, question_answer } = form;
 
@@ -59,9 +75,7 @@ const QuestionToMeDetail = () => {
         console.log("이전질문", form);
 
     }
-
-    
-
+ 
     return (
         <form>
             <label>{form.question_seq+1}. {form.question_context}</label><br/>
