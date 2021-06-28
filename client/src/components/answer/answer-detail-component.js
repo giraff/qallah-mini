00ba2e3895b87278856d6011308b79f6f302aa31
giraff <hse05105@naver.com>
2021-06-28 22:22:25 +0900

    answer 상세 페이지 (detail) 관련 컴포넌트 추가

---
+++ client/src/components/answer/answer-detail-component.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { ANSWER_DETAIL_LOAD_REQUEST } from 'redux/types';
+import AnswerDayList from './answer-day-list';
+
+const AnswerDetailComponent = ({ query }) => {
+    const { year, month, day, type } = query;
+    const dispatch = useDispatch();
+    const { answerDetail } = useSelector(state => state.answer);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const body = { year, month, day, type, token };
+
+        dispatch({
+            type: ANSWER_DETAIL_LOAD_REQUEST,
+            payload: body,
+        });
+    }, []);
+    return (
+        <div className="my-answer-container">
+            <div className="my-answer-title">나의 답변</div>
+            <div className="my-answer-content">
+                <div className="my-answer-header">
+                    <Link className="nav-btn btn-pre" to="/profile/answer">
+                        <i className="fas fa-chevron-left fa-2x" />
+                    </Link>
+                    <div className="title-text">
+                        {type === 'answerbyme' ? '내가 보는 나' : ''}
+                        {type === 'answerbyothers' ? '남이 보는 나' : ''}
+                        {type === 'answerforrefl' ? '오늘의 성찰' : ''}
+                    </div>
+                    <div className="date-text">{`${year}/${month}/${day}`}</div>
+                </div>
+                <div className="answers-wrapper">
+                    {answerDetail &&
+                        answerDetail !== '' &&
+                        answerDetail.map(detail => (
+                            <div key={detail.seq} className="answers">
+                                <div className="question-field">{`Q${detail.seq}. ${detail.question}`} </div>
+                                <div className="answer-field">
+                                    <div className="answer-elem">{`${detail.answer}`}</div>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AnswerDetailComponent;
--- client/src/pages/account/answerdetail.js
+++ client/src/pages/account/answerdetail.js
@@ -1,71 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import queryString from 'query-string';
 import ProfileCommon from './profilecommon';
 import ProfileLogout from './proflielogout';
+import AnswerDetailComponent from '../../components/answer/answer-detail-component';
 
-const AnswerDetail = () => (
-    <section className="sections account-section">
-        <div className="sections-overlay">
-            <div className="account-container">
-                <div className="sidebar">
-                    <ProfileCommon />
-                    <div className="profile-tabs">
-                        <Link className="profile-tab answer-tab" to="/profile/answer">
-                            나의 답변
-                        </Link>
-                        <Link className="profile-tab account-tab" to="/profile">
-                            나의 계정
-                        </Link>
-                    </div>
-                    <div className="logout-button">
-                        <i className="fas fa-power-off" />
-                        <ProfileLogout />
-                    </div>
-                </div>
-                <div className="my-answer-container">
-                    <div className="my-answer-title">나의 답변</div>
-                    <div className="my-answer-content">
-                        <div className="my-answer-header">
-                            <Link className="nav-btn btn-pre" to="/profile/answer">
-                                <i className="fas fa-chevron-left fa-2x" />
+const AnswerDetail = ({ location }) => {
+    const { search } = location;
+    const query = queryString.parse(search);
+    return (
+        <section className="sections account-section">
+            <div className="sections-overlay">
+                <div className="account-container">
+                    <div className="sidebar">
+                        <ProfileCommon />
+                        <div className="profile-tabs">
+                            <Link className="profile-tab answer-tab" to="/profile/answer">
+                                나의 답변
                             </Link>
-                            <div className="title-text">내가 보는 나</div>
-                            <div className="date-text">2020.09.06</div>
+                            <Link className="profile-tab account-tab" to="/profile">
+                                나의 계정
+                            </Link>
                         </div>
-                        <div className="answers-wrapper">
-                            <div className="answers">
-                                <div className="question-field">Q1. 나의 장점은?</div>
-                                <div className="answer-field">
-                                    <div className="answer-elem">- 처음 본 사람들에게 상냥하다</div>
-                                    <div className="answer-elem">- 정직하려고 노력한다.</div>
-                                    <div className="answer-elem">- 매사 민폐는 안 끼칠 만큼 좋은 결과를 낸다</div>
-                                </div>
-                            </div>
-                            <div className="answers">
-                                <div className="question-field">Q2. 나의 단점은?</div>
-                                <div className="answer-field">
-                                    <div className="answer-elem">- 게으르다</div>
-                                    <div className="answer-elem">- 신경질적이다.</div>
-                                    <div className="answer-elem">- 한 번 친해지면 막 대한다.</div>
-                                    <div className="answer-elem">- 지각을 자주한다.</div>
-                                    <div className="answer-elem">- 이기적이다.</div>
-                                    <div className="answer-elem">- 평소 자신의 일에 대해 감정적인 반면 남의 일엔 냉정하다</div>
-                                </div>
-                            </div>
-                            <div className="answers">
-                                <div className="question-field">Q3. 나의 장점은?</div>
-                                <div className="answer-field">
-                                    <div className="answer-elem">- 처음 본 사람들에게 상냥하다</div>
-                                    <div className="answer-elem">- 정직하려고 노력한다.</div>
-                                    <div className="answer-elem">- 매사 민폐는 안 끼칠 만큼 좋은 결과를 낸다</div>
-                                </div>
-                            </div>
+                        <div className="logout-button">
+                            <i className="fas fa-power-off" />
+                            <ProfileLogout />
                         </div>
                     </div>
+                    <AnswerDetailComponent query={query} />
                 </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
+};
 
 export default AnswerDetail;
