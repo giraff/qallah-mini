8c89fa19e4eb2a2d2df5e728894d501d14b38207
giraff <hse05105@naver.com>
2021-07-09 00:13:29 +0900

    나의 계정, 나의 답변 , 상세 답변 페이지 CSS 수정

--- client/src/pages/account/answermain.js
+++ client/src/pages/account/answermain.js
@@ -6,36 +6,39 @@ import AnswerList from '../../components/answer/answerlist';
 import AnswerTab from '../../components/answer/answertab';
 
 const AnswerMain = () => (
-    <section className="sections account-section">
-        <div className="sections-overlay">
-            <div className="account-container">
-                <div className="sidebar">
-                    <ProfileCommon />
-                    <div className="profile-tabs">
-                        <Link className="profile-tab answer-tab" to="/profile/answer">
-                            나의 답변
-                        </Link>
-                        <Link className="profile-tab account-tab" to="/profile">
-                            나의 계정
-                        </Link>
-                    </div>
-                    <div className="logout-button">
-                        <i className="fas fa-power-off" />
-                        <ProfileLogout />
+    <>
+        {/* banner */}
+        <section className="banner-section" />
+        <div id="account-wrap">
+            <section className="account-page-header">
+                <div className="acc-title">나의 답변</div>
+                <div className="acc-header">답변 메인</div>
+            </section>
+            <aside className="sidebar">
+                <ProfileCommon />
+                <div className="profile-tabs">
+                    <Link className="profile-tab answer-tab" to="/profile/answer">
+                        나의 답변
+                    </Link>
+                    <Link className="profile-tab account-tab" to="/profile">
+                        나의 계정
+                    </Link>
+                </div>
+                <div className="logout-button">
+                    <i className="fas fa-power-off" />
+                    <ProfileLogout />
+                </div>
+            </aside>
+            <section className="account-form">
+                <div className="user-ans-content lang-kor">
+                    <AnswerTab />
+                    <div className="category-wrapper">
+                        <AnswerList />
                     </div>
                 </div>
-                <div className="answer-content lang-kor">
-                    <div className="user-ans-title">나의 답변</div>
-                    <div className="user-ans-content">
-                        <AnswerTab />
-                        <div className="category-wrapper">
-                            <AnswerList />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </section>
         </div>
-    </section>
+    </>
 );
 
 export default AnswerMain;
