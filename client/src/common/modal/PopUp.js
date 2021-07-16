b5b0221f7b2270e7fd7f11c963474c2b714f2879
giraff <hse05105@naver.com>
2021-07-16 12:48:22 +0900

    팝업 텍스트 수정

--- client/src/common/modal/PopUp.js
+++ client/src/common/modal/PopUp.js
@@ -38,6 +38,13 @@ const PopUp = ({ modal, setModal, FirstPageLink, routerLink }) => {
                     <div className="modal">
                         <div className="popup-content">
                             <div>이전 답변 불러오시겠습니까?</div>
+                            <div className="q-byother-warning lang-kor">
+                                1일 1개의 답변 그룹만 작성 가능{' '}
+                                <p style={{ lineHeight: '25px' }}>
+                                    <span style={{ color: 'rgb(221, 41, 41)' }}>[아니오]</span> 누를 경우 당일 작성한 답변 모두{' '}
+                                    <span style={{ color: 'rgb(221, 41, 41)' }}>초기화</span>
+                                </p>
+                            </div>
                         </div>
                         <div className="popup-button-wrap">
                             <button type="button" onClick={() => loadAnswer(false)}>
